7cdc953b869a99a0ea79d7734ac175a2c1bfc8e7
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-24 03:03:05 +0600

    #13 : Memento Pattern

---
+++ MementoPattern/EditorMemento.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization;
+
+namespace Lab12_TextEditor;
+
+public class EditorMemento : ISerializable
+{
+    public string _text;
+    public int _cursorPosition;
+    public string _selection;
+    
+    public EditorMemento(string text, int cursorPosition, string selection)
+    {
+        _text = text;
+        _cursorPosition = cursorPosition;
+        _selection = selection;
+    }
+    
+    public string GetText()
+    {
+        return _text;
+    }
+    
+    public int GetCursorPosition()
+    {
+        return _cursorPosition;
+    }
+    
+    public string GetSelection()
+    {
+        return _selection;
+    }
+    
+    
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        info.AddValue("Text", _text);
+        info.AddValue("CursorPosition", _cursorPosition);
+        info.AddValue("Selection", _selection);
+    }
+}
---
+++ MementoPattern/History.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
+
+namespace Lab12_TextEditor;
+
+public class History
+{
+    private List<EditorMemento> _mementos;
+    private Editor _editor;
+    
+    public History(Editor editor)
+    {
+        _editor = editor;
+        _mementos = new List<EditorMemento>();
+    }
+    
+    public void Backup()
+    {
+        _mementos.Add(_editor.CreateMemento());
+    }
+    
+    public void Undo()
+    {
+        if (_mementos.Count == 0)
+        {
+            return;
+        }
+        var memento = _mementos.Last();
+        _mementos.Remove(memento);
+        _editor.Restore(memento);
+    }
+
+
+    public EditorMemento GetLastMemento()
+    {
+        return _mementos[_mementos.Count - 1];
+    }
+
+
+    public void SaveToJson(string path)
+    {
+        var json = JsonConvert.SerializeObject(_mementos[_mementos.Count - 1]);
+        File.WriteAllText(path, json);
+    }
+    
+    
+    public void LoadFromJson(string path)
+    {
+        var json = File.ReadAllText(path);
+        var memento = JsonConvert.DeserializeObject<EditorMemento>(json);
+        _editor.Restore(memento);
+    }
+    
+
+}
