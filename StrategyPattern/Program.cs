75e21ff53853d62752e81dc3a4b4c474e2a3e99a
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-04-09 18:41:12 +0600

    added factory to strategy

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using StrategyPattern.ConcreteStrategies;
+﻿using StrategyPattern;
+using StrategyPattern.ConcreteStrategies;
 using StrategyPattern.ContextClass;
 using StrategyPattern.Interface;
 
@@ -6,30 +7,7 @@ class main
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter route choice (air, train, walking, drive): ");
-        
-        string routeChoice = Console.ReadLine();
-        
-        IRouteStrategy routeStrategy = null;
-        
-        switch (routeChoice)
-        {
-            case "air":
-                routeStrategy = new AirRouteStrategy();
-                break;
-            case "train":
-                routeStrategy = new TrainRouteStrategy();
-                break;
-            case "walking":
-                routeStrategy = new WalkingRouteStrategy();
-                break;
-            case "drive":
-                routeStrategy = new DrivingRouteStrategy();
-                break;
-            default:
-                Console.WriteLine("Invalid route choice.");
-                break;
-        }
+        IRouteStrategy routeStrategy = RouteFactory.GetRouteStrategy("air");
 
         if (routeStrategy != null)
         {
---
+++ StrategyPattern/RouteFactory.cs
@@ -0,0 +1,37 @@
+﻿using StrategyPattern.Interface;
+using StrategyPattern.ConcreteStrategies;
+
+namespace StrategyPattern;
+
+public class RouteFactory
+{
+    public static IRouteStrategy GetRouteStrategy(string routeType)
+    {
+        Console.WriteLine("Enter route choice (air, train, walking, drive): ");
+        
+        string routeChoice = Console.ReadLine();
+        
+        IRouteStrategy routeStrategy = null;
+        
+        switch (routeChoice)
+        {
+            case "air":
+                routeStrategy = new AirRouteStrategy();
+                break;
+            case "train":
+                routeStrategy = new TrainRouteStrategy();
+                break;
+            case "walking":
+                routeStrategy = new WalkingRouteStrategy();
+                break;
+            case "drive":
+                routeStrategy = new DrivingRouteStrategy();
+                break;
+            default:
+                Console.WriteLine("Invalid route choice.");
+                break;
+        }
+        
+        return routeStrategy;
+    }
+}
